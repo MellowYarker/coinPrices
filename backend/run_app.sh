05d9cc8b5ca42a16f9a58a102c1f0369942a556c
Milan Miladinovic <milanmiladinovic@outlook.com>
2021-10-15 08:54:52 -0400

    Better run script, spawns + terms crawler/server.

--- backend/run_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python3 server.py &
-echo "server PID: $!"
-
-python3 crawler.py &
-echo "Crawler PID: $!"
--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 """Use instead of `python3 -m http.server` when you need CORS"""
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import signal, sys
 
 
 class CORSRequestHandler(SimpleHTTPRequestHandler):
@@ -10,7 +11,14 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
         self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
         return super(CORSRequestHandler, self).end_headers()
 
-if __name__ == "__main__":
+# Quiet termination
+def signal_handler(sig, frame):
+    sys.exit(0)
+
+def runServer():
+    # Signal handling
+    signal.signal(signal.SIGINT, signal_handler)
+
     httpd = HTTPServer(('localhost', 8000), CORSRequestHandler)
     httpd.serve_forever()
 
